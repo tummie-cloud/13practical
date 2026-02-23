0aa865b4bb1703682e134a162eb4bad4c3bc8d7a
tummie-cloud <4496803@myuwc.ac.za>
2026-02-23 14:43:47 +0200

    Update N value and add main method structure

--- 13template.java
+++ 13template.java
@@ -3,5 +3,22 @@ import java.io.*;
 import java.text.*;
 
 public class timeMethods{
-  public static int N=
+  public static int N=32654;
+  public static void main(String args []){
+
+    DecimalFormat twoD= new DecimalFormat("0.00");
+    DecimalFormat fourD= new DecimalFormat("0.0000");
+    DecimalFormart fiveD= new DecimalFormat("0.00000");
+
+    long start, finish;
+    double runTime = 0 , runTime2= 0 , time;
+    double totalTime = 0.0 ;
+    int n=N;
+    int repetittion , repetitions = 30;
+
+    
+    
+  }
+}  
+  
 
